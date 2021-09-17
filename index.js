dc9c7c35ca6d4f9788e532feaef0c3c707cc3b88
czyan <czyang2002@gmail,com>
2021-09-17 14:57:00 +0800

    working thing

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const { google } = require("googleapis");
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
 
 const spreadsheetID = "1ttD1tC1MO1Ab_mRl6roKoG5zx9-9VW4URZbQfMV7etw";
-
 const app = express();
 
-function name(params) {}
+const bot = new Telegraf(process.env.TOKEN);
+bot.on("text", (ctx) => {
+  console.log(ctx.message.text);
+  console.log(getRow[`/help`]);
+  ctx.reply("Welcome");
+});
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 app.get("/", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
@@ -35,6 +45,14 @@ app.get("/", async (req, res) => {
 
   res.send(getRow);
   console.log(getRow[`/help`]);
+  const bot = new Telegraf(process.env.TOKEN);
+  bot.on("text", (ctx) => {
+    ctx.reply(getRow[ctx.message.text]);
+  });
+  bot.launch();
+
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
   getResponse = () => {};
 });
